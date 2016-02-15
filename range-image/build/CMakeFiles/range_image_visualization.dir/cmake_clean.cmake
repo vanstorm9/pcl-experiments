7595daeebe3b23815aabc9964a4ff3ab669bdd02
Anthony Lowhur <antlowhur@yahoo.com>
2016-02-14 22:36:02 -0500

    Finally got openni_grabber_copyy and cloud_viewer to work

---
+++ range-image/build/CMakeFiles/range_image_visualization.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/range_image_visualization.dir/range_image_visualization.cpp.o"
+  "range_image_visualization.pdb"
+  "range_image_visualization"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/range_image_visualization.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
